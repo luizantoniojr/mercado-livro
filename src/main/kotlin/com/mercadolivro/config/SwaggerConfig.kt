173a3d9dd6f369e82f0746d8ed3ac8a171b6e65f
Luiz Antônio da Silva Júnior <luizantoniosilvajr@outlook.com>
2025-05-15 16:47:12 -0300

    Add configuration files for development environment and Swagger setup

--- src/main/kotlin/com/mercadolivro/config/SwaggerConfig.kt
+++ src/main/kotlin/com/mercadolivro/config/SwaggerConfig.kt
@@ -6,7 +6,9 @@ import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
+@Profile("dev")
 @Configuration
 class SwaggerConfig {
 
