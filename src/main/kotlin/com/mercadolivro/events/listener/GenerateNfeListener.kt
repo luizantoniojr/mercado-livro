81d464b962772d91bd9d81aba0972ecc4dfe8dbe
Luiz Antônio da Silva Júnior <luizantoniosilvajr@outlook.com>
2025-05-16 16:34:18 -0300

    Implement GenerateNfeListener and add unit tests for Nfe generation on purchase events

--- src/main/kotlin/com/mercadolivro/events/listener/GenerateNfeListener.kt
+++ src/main/kotlin/com/mercadolivro/events/listener/GenerateNfeListener.kt
@@ -5,7 +5,7 @@ import com.mercadolivro.service.PurchaseService
 import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
-import java.util.UUID
+import java.util.*
 
 @Component
 class GenerateNfeListener(
@@ -16,7 +16,6 @@ class GenerateNfeListener(
     @EventListener
     fun listen(purchaseEvent: PurchaseEvent) {
         val nfe = UUID.randomUUID().toString()
-
         val purchaseModel = purchaseEvent.purchaseModel.copy(nfe = nfe)
         purchaseService.update(purchaseModel)
     }
